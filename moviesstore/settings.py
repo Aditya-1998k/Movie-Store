39e4b872e237664e4e3e9291d7e203e8f29a0b8c
aditya <aditya98gupta@gmail.com>
2025-11-09 09:53:14 +0530

    Production ready

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -134,3 +134,14 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'moviesstore/static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
